fe394ad0d23ffc903e7fcf3028ee9b467afbc236
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-12-10 14:01:42 +0100

    add: create Header and Footer components

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer(){
+
+    return (
+
+        <footer>
+            <section>
+                <div className="container">
+                    <h2>My Reporter Blog </h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis, impedit?</p>
+                </div>
+            </section>
+        </footer>
+    )
+}
---
+++ src/layouts/Header.jsx
@@ -0,0 +1,15 @@
+
+import Navbar from "../components/Navbar"
+
+export default function Header() {
+    return (
+      <header>
+        <div className="container">
+          <div>
+            Logo
+          </div>
+          <Navbar />
+        </div>
+      </header>
+    )
+  }
