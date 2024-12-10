c1f304f9825988e8550468ddff75b0f8ad163de3
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-12-10 14:28:23 +0100

    add: default layout component added

---
+++ src/layouts/DefaultLayout.jsx
@@ -0,0 +1,17 @@
+
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import { Outlet } from "react-router-dom";
+
+export default function DefaultLayout(){
+
+    return (
+
+        <div className="layout">
+            <Header />
+            <Outlet />
+            <Footer />
+        </div>
+
+    )
+}
